9e06ae913eb4368583e4b8d81042b1a31f8081fa
vishalumare <vishaldemo2004@gmail.com>
2026-01-17 09:12:09 +0530

    Learned about the Interpolation syntax

--- NodeEJS/index.js
+++ NodeEJS/index.js
@@ -13,6 +13,20 @@ app.get("/" ,(req,res) =>{
 //     res.send("server running displayed")
 // })
 
+app.get("/rolldice" , (req,res) =>{
+    let diceVal = Math.floor(Math.random() * 6) +1 ;
+    res.render("rolldice.ejs" , {diceVal})
+})
+
+
+// Activity insta page
+app.get("/ig/:username" , (req,res) =>{
+    const followers = ["Adam", "Cris", "Bob", "Hadden"]
+    let {username} = req.params ;
+    res.render("instagram.ejs" , {username , followers})
+})
+
+
 app.listen(port , ()=>{
     console.log(`App listening on port ${port}`)
 });
