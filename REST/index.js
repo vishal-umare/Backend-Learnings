70c913870b6b8cfcbea39d850d24c8d18bf8195c
vishalumare <vishaldemo2004@gmail.com>
2026-01-21 22:07:54 +0530

    Activity-RESTFUL APIs

--- REST/index.js
+++ REST/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const port = 4000;
 const path = require("path")
+const { v4: uuidv4 } = require('uuid');
 
 app.use(express.urlencoded({extended:true}));
 app.set("view engine", "ejs")
@@ -12,14 +13,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts  =[
     {
+        id: uuidv4(),
         username : "vishal",
         content :"I love coding"
     },
     {
+        id: uuidv4(),
         username : "harsh",
         content :"hardwork is needed for success"
     },
     {
+        id: uuidv4(),
         username : "nikhil",
         content :"got my first internship"
     }
@@ -38,13 +42,30 @@ app.get("/posts/new", (req,res) =>{
 
 app.post("/posts", (req,res) =>{
     let {username , content} = req.body ;
-    posts.push({ username, content });
+    let id = uuidv4();
+    posts.push({ id, username, content });
     res.redirect("/posts")                           //new info REDIRECT to main page 
 })
 
 
+// SHOW ROUTE(IMPLEMETNT: GET/POSTS/:ID)
+app.get("/posts/:id", (req,res) =>{
+    let {id} = req.params ;
+    let post = posts.find( (p)=> id === p.id );
+    res.render("show.ejs" , {post})
+})
+
+
+// UPDATE ROUTE(IMPLEMETNT: PATCH/POSTS/:ID)
+app.patch("/posts/:id" ,(req,res) =>{
+    let {id} = req.params ;
+    let newContent = req.body.content;
+    let post = posts.find( (p)=> id === p.id );
+    post.content = newContent;
+    console.log(post)
+    res.send("patch is working")
+})
 
 app.listen(port, ()=>{
     console.log("Listening to the port");
 })
- 
