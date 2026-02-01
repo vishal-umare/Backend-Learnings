38f4316af4431e1040e4ae9d5f1d37bf37474070
vishalumare <vishaldemo2004@gmail.com>
2026-02-01 10:19:21 +0530

    feat: implement hybrid REST API with mock data integration

--- Revision/index.js
+++ Revision/index.js
@@ -1,6 +1,6 @@
 
 
-const fs = require("fs");
+// const fs = require("fs");
 
 // SYNC (READING THE FILE)
 // const result = fs.readFileSync("./contacts.txt" , "utf-8");
@@ -17,4 +17,28 @@ const fs = require("fs");
 
 // SYNCHRONOUS (APPENDING)
 
-fs.appendFileSync("./text.txt", `heyy there \n`)
+// fs.appendFileSync("./text.txt", `heyy there \n`)
+
+// ---------------------------------------------------------------------------------------------
+
+// **CREATING SIMPLE PROJECT
+
+const express = require("express");
+const app = express() ;
+const users = require("./MOCK_DATA.json")
+const PORT = 5000 ;
+
+
+app.get("/api/users",(req,res) => {
+    res.json(users);
+})
+
+app.get("/api/users/:id",(req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find( (user)=> user.id === id) ;
+    res.json(user);
+})
+
+app.listen(PORT, ()=>{
+    console.log(`App is listening on ${PORT}`)
+})
