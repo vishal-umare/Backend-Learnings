af9631532d0ef99bc8a105ea88244e36ac8c57e9
vishalumare <vishaldemo2004@gmail.com>
2026-02-01 14:28:00 +0530

    feat: add basic middleware skeletons and syntax examples

--- Revision/index.js
+++ Revision/index.js
@@ -26,9 +26,19 @@
 const express = require("express");
 const app = express() ;
 const users = require("./MOCK_DATA.json")
+const fs = require("fs");
 const PORT = 5000 ;
 
+// Middleware Plugin
+app.use(express.urlencoded({extended:false}));
 
+app.use( (req, res, next) =>{
+    console.log("Hello from middleware");
+    res.json("hello");
+    next();
+})
+
+// routes
 app.get("/api/users",(req,res) => {
     res.json(users);
 })
@@ -39,6 +49,17 @@ app.get("/api/users/:id",(req,res)=>{
     res.json(user);
 })
 
+
+app.post("/api/users",(req,res)=>{
+    const body = req.body ;
+    users.push({...body , id:users.length + 1 });
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users) , (err, data) =>{
+        res.json({ status:"success", id:users.length})
+    })
+})
+
+
+// Listen
 app.listen(PORT, ()=>{
     console.log(`App is listening on ${PORT}`)
-})
+})  
