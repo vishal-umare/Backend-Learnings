4d33b7584911be1e0fe6a85627cb0bcd7bc507ab
vishalumare <vishaldemo2004@gmail.com>
2026-02-05 21:48:37 +0530

    Implement SQL schema and data seeding via Node.js CLI

--- SQL/index.js
+++ SQL/index.js
@@ -1,38 +1,65 @@
-const { faker } = require('@faker-js/faker');
-const mysql      = require('mysql2');
+const { faker } = require("@faker-js/faker");
+const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'delta_app'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "delta_app",
 });
 
-// Inserting New Data 
-let q = "INSERT INTO users (id, username, email, password) VALUES (?, ?, ?, ?)" ;
-let users = ["123", "123_newuser", "abc@gmail.com", "abc"]
+// let getRandomUser = () => {
+//   return {
+//     id: faker.string.uuid(),
+//     username: faker.internet.username(),
+//     email: faker.internet.email(),
+//     password: faker.internet.password(),
+//   };
+// };
 
-try{
-    connection.query(q, users, (error, results) =>{
-    if(error) throw error ;
-    console.log(results)
-    }) 
+let getRandomUser = () => {
+  return [
+    faker.string.uuid(),
+    faker.internet.username(),
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
+};
+
+console.log(getRandomUser());
+
+
+
+
+// Inserting New Data
+
+// let q = "INSERT INTO users (id, username, email, password) VALUES ?";
+// let users = ["123", "123_newuser", "abc@gmail.com", "abc"]
+
+// let users = [
+//   ["123b", "123_newuserb", "abc@gmail.comb", "abcb"],
+//   ["123c", "123_newuserc", "abc@gmail.comc", "abcc"],
+// ];
+
+
+
+// Inserting 100 fake user 
+let q = "INSERT INTO users (id, username, email, password) VALUES ?";
+
+let data = [];
+for(let i=1; i<=100; i++){
+  data.push(getRandomUser());
 }
-catch(error){
-    console.log(error);
+
+
+try {
+  connection.query(q, [data], (error, results) => {
+    if (error) throw error;
+    console.log(results);
+  });
+} catch (error) {
+  console.log(error);
 }
 
 connection.end();
 
-
-
-let getRandomUser = ()=> {
-  return {
-    id: faker.string.uuid(),
-    username: faker.internet.username(),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-  };
-}
-
-// console.log(getRandomUser());
