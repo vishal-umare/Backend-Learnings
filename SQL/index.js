2ec3595759ff05efa1bffa8c5439d27ffcf3553d
vishalumare <vishaldemo2004@gmail.com>
2026-02-07 21:44:34 +0530

    Implement user edit and update functionality with MySQL

--- SQL/index.js
+++ SQL/index.js
@@ -66,7 +66,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 require("dotenv").config();
+const methodOverride = require("method-override");
 
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -86,13 +89,16 @@ let getRandomUser = () => {
   ];
 };
 
+// ROUTES
+// Showing Total users count
 app.get("/", (req, res) => {
   let q = `SELECT count(*) FROM users ;`
   try {
     connection.query(q, (error, result) => {
       if (error) throw error;
-      console.log(result);
-      res.render("home.ejs")
+      // console.log(result);
+      let count = result[0]["count(*)"] ;
+      res.render("home.ejs", {count})
     });
   } 
   catch (error) {
@@ -100,6 +106,67 @@ app.get("/", (req, res) => {
     res.send("Some error in DB")
   }
 });
+
+// Showing users
+app.get("/user", (req, res) =>{
+  let q = `SELECT * FROM users ;`
+  try {
+    connection.query(q, (error, result) => {
+      if (error) throw error;
+      let users = result;
+      res.render("showusers.ejs", {users})
+    });
+  } 
+  catch (error) {
+    console.log(error);
+    res.send("Some error in DB")
+  }
+})
+
+// Edit username
+app.get("/user/:id/edit" ,(req,res)=>{
+  let {id} = req.params ;
+  let q = `SELECT * FROM users WHERE id = '${id}'`
+  try {
+    connection.query(q, (error, result) => {
+      if (error) throw error;
+      let user = (result[0]);
+      res.render("edit.ejs", {user})
+    });
+  } 
+  catch (error) {
+    console.log(error);
+    res.send("Some error in DB")
+  }
+})
+
+// Update (DB) route
+app.patch("/user/:id", (req,res)=>{
+  let {id} = req.params ;
+  let { password : formPass , username : newUsername} = req.body ;
+  let q = `SELECT * FROM users WHERE id = '${id}'`
+
+  try {
+    connection.query(q, (error, result) => {
+      if (error) throw error;
+      let user = (result[0]);
+      if(formPass =! user.password) {
+        res.send("Wrong Password entered")
+      }
+      else{
+        let q2 = `UPDATE users SET username='${newUsername}' WHERE id = '${id}'`
+        connection.query(q2, (error, result)=>{
+          if (error) throw error;
+          res.redirect("/user")
+        })
+      }
+    });
+  } 
+  catch (error) {
+    console.log(error);
+    res.send("Some error in DB")
+  }
+})
 
 app.listen("8080", () => {
   console.log("App is listening on port 8080");
