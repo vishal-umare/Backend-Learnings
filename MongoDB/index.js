6dea0034f31a99f27794e193e83d2d4d450f1599
vishalumare <vishaldemo2004@gmail.com>
2026-02-21 11:58:39 +0530

    Initialize MongoDB learning

---
+++ MongoDB/index.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+main()
+  .then(() => {
+    console.log("Connection success");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+}
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number
+})
+
+const User = mongoose.model("User", userSchema);
