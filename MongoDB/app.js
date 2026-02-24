f5f3f56bf21f5651513d249355c2ccd147b55c54
vishalumare <vishaldemo2004@gmail.com>
2026-02-24 17:37:16 +0530

    add message edit and delete routes with MongoDB persistence

--- MongoDB/app.js
+++ MongoDB/app.js
@@ -3,11 +3,14 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat");
+const methodOverride = require('method-override');  
+
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 
 main()
   .then((res) => console.log("Connection Successfull"))
@@ -45,6 +48,30 @@ app.post("/chats", (req, res) => {
 
   res.redirect("/chats")
 });
+
+// EDIT ROUTE
+app.get("/chats/:id/edit", async (req,res) =>{
+  let {id} = req.params ;
+  let chat = await Chat.findById(id);
+  res.render("edit.ejs",{chat})
+})
+
+// UPDATE (PUT REQ)
+app.put("/chats/:id", async (req,res) =>{
+  let {id} = req.params ;
+  let {msg: newMsg} = req.body ;
+  let updatedChat = await Chat.findByIdAndUpdate(id, {msg: newMsg}, {runValidators: true, new:true})
+  console.log(updatedChat);
+  res.redirect("/chats");
+})
+
+// DESTROY ROUTE
+app.delete("/chats/:id", async (req,res) =>{
+  let {id} = req.params ;
+  let deletedChat = await Chat.findByIdAndDelete(id) ;
+  console.log(deletedChat);
+  res.redirect("/chats"); 
+})
 
 app.listen(8080, () => {
   console.log("server is listening");
