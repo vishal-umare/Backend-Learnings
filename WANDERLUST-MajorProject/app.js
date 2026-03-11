5ae884891c462d97a045b596fede8942de1d05a3
vishalumare <vishaldemo2004@gmail.com>
2026-03-11 21:53:55 +0530

    implement core CRUD routes for property listings

--- WANDERLUST-MajorProject/app.js
+++ WANDERLUST-MajorProject/app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -32,10 +38,33 @@ app.get("/", (req, res) => {
 //     res.send("Testing W")
 // })
 
-app.get("/listings", (req, res) => {
-  Listing.find({}).then((res) => console.log(res));
+// Index Route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
+
+// New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+// Show Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+// Create Route
+app.post("/listings", async (req, res) =>{
+  // let { title, description, image, price, location, country} = req.body ;
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings")
+})
 
 app.listen(8080, () => {
   console.log("App is listening on 8080");
 });
+  
