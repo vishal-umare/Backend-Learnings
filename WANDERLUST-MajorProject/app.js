2dd7338b1eb03439e46d7339fbbe0dcec693fe24
vishalumare <vishaldemo2004@gmail.com>
2026-03-12 10:24:19 +0530

    finalize full CRUD lifecycle for listings

--- WANDERLUST-MajorProject/app.js
+++ WANDERLUST-MajorProject/app.js
@@ -3,11 +3,12 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -62,6 +63,27 @@ app.post("/listings", async (req, res) =>{
   const newListing = new Listing(req.body.listing);
   await newListing.save();
   res.redirect("/listings")
+})
+
+// Edit Route 
+app.get("/listings/:id/edit", async (req, res) =>{
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", {listing});
+})
+
+// Update Route
+app.put("/listings/:id" , async (req, res) =>{
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, {...req.body.listing})
+  res.redirect("/listings")
+})
+
+// Delete Route
+app.delete("/listings/:id", async (req, res) =>{
+  let { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
 })
 
 app.listen(8080, () => {
