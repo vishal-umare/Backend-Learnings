032f79f40df60f03a32f7a1931e87e0e7ef50c7c
vishalumare <vishaldemo2004@gmail.com>
2026-03-16 21:27:04 +0530

    implement centralized error handling and Joi schema validation

--- WANDERLUST-MajorProject/app.js
+++ WANDERLUST-MajorProject/app.js
@@ -5,6 +5,9 @@ const Listing = require("./models/listing");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate")
+const wrapAsync = require("./utils/wrapAsync.js")
+const ExpressError = require("./utils/ExpressError.js")
+const {listingSchema}  = require("./schema.js")
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -29,6 +32,16 @@ app.get("/", (req, res) => {
   res.send("Hiii");
 });
 
+const validateListing = (req, res, next) =>{
+  let {error} = listingSchema.validate(req.body);
+
+    if(error){
+      let errMsg = error.details.map((el) => el.message).join(",");
+      throw new ExpressError(400, errMsg);
+    }else{
+      next();
+    }
+}
 // app.get("/testListing", async (req, res) =>{
 //     const sample = new Listing({
 //         title: "New Villa",
@@ -43,10 +56,10 @@ app.get("/", (req, res) => {
 // })
 
 // Index Route
-app.get("/listings", async (req, res) => {
+app.get("/listings",  wrapAsync(async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", { allListings });
-});
+}));
 
 // New Route
 app.get("/listings/new", (req, res) => {
@@ -54,50 +67,55 @@ app.get("/listings/new", (req, res) => {
 });
 
 // Show Route
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id",  wrapAsync(async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/show.ejs", { listing });
-});
+}));
 
 // Create Route
-app.post("/listings", async (req, res, next) =>{
+app.post("/listings", validateListing, wrapAsync(async (req, res, next) =>{
   // let { title, description, image, price, location, country} = req.body ;
-  try{
+    
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings")
-  }catch(err){
-    next(err);
-  }
-})
+}));
 
 // Edit Route 
-app.get("/listings/:id/edit", async (req, res) =>{
+app.get("/listings/:id/edit",  wrapAsync(async (req, res) =>{
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/edit.ejs", {listing});
-})
+}));
 
 // Update Route
-app.put("/listings/:id" , async (req, res) =>{
+app.put("/listings/:id" , validateListing,  wrapAsync(async (req, res) =>{
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, {...req.body.listing})
   res.redirect("/listings")
-})
+}));
 
 // Delete Route
-app.delete("/listings/:id", async (req, res) =>{
+app.delete("/listings/:id",  wrapAsync(async (req, res) =>{
   let { id } = req.params;
   await Listing.findByIdAndDelete(id);
   res.redirect("/listings");
+}));
+
+// Error
+app.use((req, res, next) =>{
+  next(new ExpressError(404, "Page not found!"));
 })
 
 app.use((err, req, res, next) =>{
-  res.send("something went wrong")
+  let { statusCode=500, message="Something went wrong"} = err;
+  res.status(statusCode).render("listings/error.ejs",{err})
+
+  // res.status(statusCode).send(message);
+  // res.send("something went wrong")
 })
 
 app.listen(8080, () => {
   console.log("App is listening on 8080");
 });
-  
