5ed87a65214f22b397a4fb51397c6c161d101dc6
vishalumare <vishaldemo2004@gmail.com>
2026-03-23 10:22:06 +0530

    implemented secure Signup and Login authentication routes

--- WANDERLUST-MajorProject/models/user.js
+++ WANDERLUST-MajorProject/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const passportLocalMongoose = require('passport-local-mongoose');
+const passportLocalMongoose = require('passport-local-mongoose').default;
 const userSchema = new mongoose.Schema({
     email:{
         type: String,
